394ca60594ac36b1e7b3a1b453cfcf2ef7cf97fc
Gyanesh Mishra <13112527+gyanesh-mishra@users.noreply.github.com>
2019-10-02 14:44:56 -0700

    Add docker build support (#3)

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,72 +1,77 @@
-const webpack = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+const HOST = process.env.HOST || "0.0.0.0";
+const PORT = process.env.PORT || 3000;
 
 module.exports = [
   {
-    name: 'dev',
-    entry: './src/index.js',
-    mode: 'development',
+    name: "dev",
+    entry: "./src/index.js",
+    mode: "development",
     module: {
       rules: [
         {
           test: /\.(js|jsx)$/,
           exclude: /node_modules/,
-          use: ['babel-loader'],
-        },
-      ],
+          use: ["babel-loader"]
+        }
+      ]
     },
     resolve: {
-      extensions: ['*', '.js', '.jsx'],
+      extensions: ["*", ".js", ".jsx"]
     },
     output: {
-      path: __dirname + '/dist',
-      publicPath: './',
-      filename: 'bundle.js',
+      path: __dirname + "/dist",
+      publicPath: "./",
+      filename: "bundle.js"
     },
     plugins: [
       new CleanWebpackPlugin(),
       new HtmlWebpackPlugin({
-        title: 'Basic React App',
-        template: './src/index.html',
+        title: "Basic React App",
+        template: "./src/index.html"
       }),
-      new webpack.HotModuleReplacementPlugin(),
+      new webpack.HotModuleReplacementPlugin()
     ],
     devServer: {
-      contentBase: './dist',
+      contentBase: "./dist",
       hot: true,
       historyApiFallback: true,
       writeToDisk: true,
-    },
+      host: HOST,
+      port: PORT
+    }
   },
   {
-    name: 'prod',
-    entry: './src/index.js',
-    mode: 'production',
+    name: "prod",
+    entry: "./src/index.js",
+    mode: "production",
     module: {
       rules: [
         {
           test: /\.(js|jsx)$/,
           exclude: /node_modules/,
-          use: ['babel-loader'],
-        },
-      ],
+          use: ["babel-loader"]
+        }
+      ]
     },
     resolve: {
-      extensions: ['*', '.js', '.jsx'],
+      extensions: ["*", ".js", ".jsx"]
     },
     output: {
-      path: __dirname + '/dist',
-      publicPath: './',
-      filename: 'bundle.js',
+      path: __dirname + "/dist",
+      publicPath: "./",
+      filename: "bundle.js"
     },
     plugins: [
       new CleanWebpackPlugin(),
       new HtmlWebpackPlugin({
         // TODO: Add cache-buster (if needed)
-        title: 'Basic React App',
-        template: './src/index.html',
-      }),
-    ],
-  },
+        title: "Basic React App",
+        template: "./src/index.html"
+      })
+    ]
+  }
 ];
